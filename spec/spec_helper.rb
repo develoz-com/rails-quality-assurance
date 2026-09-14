fce4d4697fbef7c117ca8b51bf28eeddbd801a70
Mauricio Zaffari <mauriciozaffari@gmail.com>
2026-09-14 16:21:39 +0000

    Add specs and lint setup for the gem itself

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rubocop'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.disable_monkey_patching!
+  config.order = :random
+end
